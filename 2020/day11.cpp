bc5586d3ce7b6cbc8b2f936c7c59d07fb6444953
samJBonnell <sbonnell147@gmail.com>
2021-10-28 18:13:51 -0700

    This is a test commit

--- 2020/day11.cpp
+++ 2020/day11.cpp
@@ -4,17 +4,10 @@
 #include <iterator>
 #include <sstream>
 
-uint64_t square(const uint64_t num) {
-    return num * num;
-}
+#include "day11.h"
 
 int main(void) {
-    uint64_t num;
+    SeatingArrangment arrangmentOne(std::cin);
 
-    std::cout << "Enter a number" << std::endl;
-    std::cin >> num;
-
-    for (uint64_t i = 1; i <= num; i++) { std::cout << square(i) << std::endl; }
-
-    return 0;
+    
 }
--- 2020/day11.h
+++ 2020/day11.h
@@ -5,18 +5,16 @@
 #include <string>
 #include <vector>
 #include <array>
+#include <sstream>
 
 class Seat {
 public:
     Seat() = default;
+    Seat(char iniState) : _State(iniState), _NextState(' ') {};
 
     void queueUpdate(std::array<Seat, 8>& adjacentSeats);
     bool returnState(void) const { return (_State == '#') ? 1 : 0; }; // If the seat is occupied, return 1, else, return 0;
     void updateState();
-    
-    static void nextStep(void) {
-
-    };
 
 private:
     char _State;
@@ -34,7 +32,49 @@ void Seat::queueUpdate(std::array<Seat, 8>& adjacentSeats) {
 
 void Seat::updateState(void) {
     this->_State = this->_NextState;
-    std::cout << "This is just for testing" << std::endl;
+    this->_NextState = ' ';
+}
+
+class SeatingArrangment {
+public:
+    SeatingArrangment() = default;
+    SeatingArrangment(std::istream& in);
+
+    void updateSeatingArrangment();
+    std::array<Seat, 8> SeatingArrangment::returnAdjacentSeats(uint8_t& row, uint8_t& col) const;
+
+private:
+    std::vector<std::vector<Seat>> Seats;
+};
+
+SeatingArrangment::SeatingArrangment(std::istream& in) {
+    std::string inputRow;
+    std::vector<Seat> rowVector;
+
+    while (std::getline(in, inputRow)) {
+        std::stringstream col(inputRow);
+
+        for (const auto& colState : inputRow) rowVector.push_back(Seat(colState));
+        Seats.push_back(rowVector);
+    }
+}
+
+void SeatingArrangment::updateSeatingArrangment() {
+    for (auto& row : Seats) for (auto& col : row) col.updateState();
+}
+
+std::array<Seat, 8> SeatingArrangment::returnAdjacentSeats(uint8_t& row, uint8_t& col) const {
+    std::array<Seat, 8> adjacentSeats;
+    int rowFactor, colFactor;
+
+    if (row - 1 < 0) rowFactor;
+    if (row + 1 > Seats.size());
+
+    if (col - 1 < 0) ;
+    if (col - 1 > Seats[0].size());
+
+    for (auto rowIT = Seats.begin() + rowFactor, endIT = Seats.begin() + 3; rowIT != endIT; rowIT);
+
 }
 
 #endif // DAY11_H
