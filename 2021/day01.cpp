7d4992d7b5be6d39a96206278ff6ad56a236b42b
samJBonnell <sbonnell147@gmail.com>
2021-12-01 09:29:38 -0800

    Day 01 Finalized

--- 2021/day01.cpp
+++ 2021/day01.cpp
@@ -2,17 +2,42 @@
 #include <iterator>
 #include <vector>
 
+void partOne(std::vector<int>::iterator it, std::vector<int>::iterator end, uint64_t& increases);
+void partTwo(std::vector<int>::iterator it, std::vector<int>::iterator end, uint64_t& increases);
+
 int main(void) {
     std::istream_iterator<int> in(std::cin), eof;
     std::vector<int> depths(in, eof);
 
-    uint64_t increases = 0;
+    uint64_t increasePartOne = 0;
+    uint64_t increasePartTwo = 0;
 
-    for (auto it = depths.begin(), offset = depths.begin() + 1, end = depths.end(); offset != end; it++, offset++) {
-        increases += (offset > it) ? 1 : 0;
-    }
+    partOne(depths.begin(), depths.end(), increasePartOne);
+    partTwo(depths.begin(), depths.end(), increasePartTwo);
 
-    std::cout << std::endl << increases << std::endl;
+    std::cout << std::endl <<  "Part One: " << increasePartOne << std::endl;
+    std::cout <<               "Part Two: " << increasePartTwo << std::endl;
 
     return 0;
+}
+
+void partOne(std::vector<int>::iterator it, std::vector<int>::iterator end, uint64_t& increases) { // Part One
+    for (auto offset = it + 1; offset != end; it++, offset++) {
+        increases += (*offset > *it) ? 1 : 0;
+    }
+
+    return;
+}
+
+void partTwo(std::vector<int>::iterator it, std::vector<int>::iterator end, uint64_t& increases) { // Part Two
+    std::vector<int>::iterator offsetOne = it + 1, offsetTwo = it + 2;
+    uint64_t currentSum, previousSum = 0;
+
+    for(;offsetTwo != end; it++, offsetOne++, offsetTwo++) {
+        currentSum = *it + *offsetOne + *offsetTwo;
+        if (previousSum) increases += (currentSum > previousSum) ? 1 : 0;
+        previousSum = currentSum;
+    }
+
+    return;
 }
