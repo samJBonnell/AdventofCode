611cdae04c115d90e5e68fbb89a9140c149d6ce2
samJBonnell <sbonnell147@gmail.com>
2021-12-03 11:22:04 -0800

    Less Pain

--- 2021/day03.cpp
+++ 2021/day03.cpp
@@ -2,7 +2,9 @@
 #include <iterator>
 #include <vector>
 #include <string>
-#include <map>
+#include <functional>
+
+uint16_t mostcommonBits(const std::vector<uint16_t>& binary, const uint16_t& inputLength, std::function<bool(const uint16_t&, const uint16_t&)> comp);
 
 int main(void) {
     std::istream_iterator<std::string> in(std::cin),  eof;
@@ -13,26 +15,15 @@ int main(void) {
         binary.push_back(std::stoi(string, 0, 2));
     }
 
+    uint16_t inputLength = input[0].length() - 1;
+
     // Part One
 
-    uint16_t oneBit = 0, zeroBit = 0;
     uint16_t gamma = 0, epsilon = 0;
 
-    for (std::string::size_type i = 0; i < input[0].length(); i++) {
-        for (const auto& number : binary) {
-            if(((number >> input[0].length() - i - 1) & 1) == 1) oneBit++;
-            else {
-                zeroBit++;
-            }
-        }
+    gamma = mostcommonBits(binary, inputLength, [](uint16_t first, uint16_t second) -> bool { return (first >= second); });
 
-        if (oneBit >= zeroBit) gamma |= 1 << (input[0].length() - i - 1);
-
-        oneBit = 0;
-        zeroBit = 0;
-    }
-
-    for (std::string::size_type i = 0; i < input[0].length(); i++) {
+    for (std::string::size_type i = 0; i <= inputLength; i++) {
         epsilon |= (~(gamma >> i) & 1) << (i);
     }
 
@@ -41,91 +32,28 @@ int main(void) {
 
     // Part Two
 
-    std::vector<uint16_t> binaryCopy;
 
-    for (const auto& element : binary) {
-        binaryCopy.push_back(element);
-    }
-
-    uint16_t index = input[0].length() - 1;
-
-    oneBit = 0;
-    zeroBit = 0;
-    gamma = 0;
-    epsilon = 0;
-
-    while (binary.size() != 1) {
-        for (std::string::size_type i = 0; i < input[0].length(); i++) {
-            for (const auto& number : binary) {
-                if(((number >> input[0].length() - i - 1) & 1) == 1) oneBit++;
-                else {
-                    zeroBit++;
-                }
-            }
-
-            if (static_cast<float>(oneBit)/binary.size() >= static_cast<float>(zeroBit)/binary.size()) gamma |= 1 << (input[0].length() - i - 1);
-
-            oneBit = 0;
-            zeroBit = 0;
-        }
-
-        // Find oxygen number
-
-        for (auto it = binary.begin(); it != binary.end() && binary.size() != 1;) {
-            if (((gamma >> index) & 1) != ((*it >> index) & 1)) {
-                it = binary.erase(it);
-            }
-            else {
-                it++;
-            }
-        }
-
-        index--;
-        
-    }
-
-    index = input[0].length() - 1;
-
-    oneBit = 0;
-    zeroBit = 0;
-    gamma = 0;
-    epsilon = 0;
-
-    while (binaryCopy.size() != 1) {
-        for (std::string::size_type i = 0; i < input[0].length(); i++) {
-            for (const auto& number : binaryCopy) {
-                if(((number >> input[0].length() - i - 1) & 1) == 1) oneBit++;
-                else {
-                    zeroBit++;
-                }
-            }
-
-            if (static_cast<float>(oneBit)/binaryCopy.size() < static_cast<float>(zeroBit)/binaryCopy.size()) gamma |= 1 << (input[0].length() - i - 1);
-
-            oneBit = 0;
-            zeroBit = 0;
-        }
-
-        for (std::string::size_type i = 0; i < input[0].length(); i++) {
-            epsilon |= (~(gamma >> i) & 1) << (i);
-        }
-
-        // Find CO2 number
-
-        for (auto it = binaryCopy.begin(); it != binaryCopy.end() && binaryCopy.size() != 1;) {
-            if (((gamma >> index) & 1) != ((*it >> index) & 1)) {
-                it = binaryCopy.erase(it);
-            }
-            else {
-                it++;
-            }
-        }
-
-        index--;
-    }
-
-    std::cout << std::endl<< binary[0] * binaryCopy[0] << std::endl;
-    std::cout << binary[0] << " " << binaryCopy[0] << std::endl;
 
     return 0;
+}
+
+uint16_t mostcommonBits(const std::vector<uint16_t>& binary, const uint16_t& inputLength, std::function<bool(const uint16_t&, const uint16_t&)> comp) {
+    uint16_t oneBit = 0, zeroBit = 0;
+    uint16_t gamma = 0, epsilon = 0;
+
+    for (std::string::size_type i = 0; i <= inputLength; i++) {
+        for (const auto& number : binary) {
+            if(((number >> inputLength - i) & 1) == 1) oneBit++;
+            else {
+                zeroBit++;
+            }
+        }
+
+        if (comp(oneBit, zeroBit)) gamma |= 1 << (inputLength - i);
+
+        oneBit = 0;
+        zeroBit = 0;
+    }
+
+    return gamma;
 }
