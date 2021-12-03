09972432aab95ab75bf16bee9c337940216240eb
samJBonnell <sbonnell147@gmail.com>
2021-12-03 12:52:34 -0800

    Comments

--- 2021/day03.cpp
+++ 2021/day03.cpp
@@ -4,48 +4,53 @@
 #include <string>
 #include <functional>
 
+// Function Prototypes
+
 uint16_t getBits(const std::vector<uint16_t>& binary, const uint16_t& inputLength, std::function<bool(const uint16_t&, const uint16_t&)> comp);
 bool nthBit(uint16_t& number, uint16_t index);
 void finalEntry(std::vector<uint16_t>& vec, uint16_t index, std::function<bool(const uint16_t&, const uint16_t&)> comp);
+
+
 
 int main(void) {
     std::istream_iterator<std::string> in(std::cin),  eof;
     std::vector<std::string> input(in, eof);
     std::vector<uint16_t> binary;
 
-    for (const auto& string : input) {
-        binary.push_back(std::stoi(string, 0, 2));
-    }
-
     uint16_t inputLength = input[0].length() - 1;
+    uint16_t gamma = 0, epsilon = 0;
+
+    for (const auto& string : input) { binary.push_back(std::stoi(string, 0, 2)); }
+    std::vector<uint16_t> binaryCopy(binary);
 
     // Part One
 
-    uint16_t gamma = 0, epsilon = 0;
-
     gamma = getBits(binary, inputLength, [](uint16_t first, uint16_t second) -> bool { return (first >= second); });
-
-    for (std::string::size_type i = 0; i <= inputLength; i++) {
-        epsilon |= (~(gamma >> i) & 1) << (i);
-    }
+    for (std::string::size_type i = 0; i <= inputLength; i++) { epsilon |= (~(gamma >> i) & 1) << (i); }
 
     // Part Two
 
-    std::vector<uint16_t> binaryCopy(binary);
+    finalEntry(binary,      inputLength, [](uint16_t first, uint16_t second) -> bool { return (first >= second); });
+    finalEntry(binaryCopy,  inputLength, [](uint16_t first, uint16_t second) -> bool { return (first < second); });
 
-    finalEntry(binary, inputLength, [](uint16_t first, uint16_t second) -> bool { return (first >= second); });
-    finalEntry(binaryCopy, inputLength, [](uint16_t first, uint16_t second) -> bool { return (first < second); });
+    // Output
 
     std::cout << std::endl << "Part One:" << std::endl << gamma * epsilon << std::endl << gamma << " " << epsilon << std::endl << std::endl;
-
     std::cout << "Part Two" << std::endl << binary[0] * binaryCopy[0] << std::endl << binary[0] << " " << binaryCopy[0] << std::endl;
 
     return 0;
 }
 
+/*
+
+Determine the most common bit at each index from a list of uint16_t's. 
+Using lambda comp(), determine if a 1 or 0 should be set in an output number based on commanality.
+
+*/
+
 uint16_t getBits(const std::vector<uint16_t>& binary, const uint16_t& inputLength, std::function<bool(const uint16_t&, const uint16_t&)> comp) {
     uint16_t oneBit = 0, zeroBit = 0;
-    uint16_t gamma = 0, epsilon = 0;
+    uint16_t gamma = 0;
 
     for (std::string::size_type i = 0; i <= inputLength; i++) {
         for (const auto& number : binary) {
@@ -64,7 +69,7 @@ uint16_t getBits(const std::vector<uint16_t>& binary, const uint16_t& inputLengt
     return gamma;
 }
 
-bool nthBit(uint16_t& number, uint16_t index) {
+bool nthBit(uint16_t& number, uint16_t index) { // Return the value of the nth bit in a number
     return ((number >> index) & 1);
 }
 
