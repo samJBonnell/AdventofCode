b3e374b1ba6af908aed7193f7bf56de75e63a592
samJBonnell <sbonnell147@gmail.com>
2021-12-10 17:16:17 -0800

    Not proud, but finished day08

---
+++ 2021/day08.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+#include "day08.hpp"
+
+constexpr uint16_t ONE      = 2;
+constexpr uint16_t FOUR     = 4;
+constexpr uint16_t SEVEN    = 3;
+constexpr uint16_t EIGHT    = 7;
+
+int main(void) {
+    std::vector<std::vector<uniqueSignal>> digitalOutputs;
+    std::vector<uniqueSignal> tempVec;
+    std::string tempStr;
+
+    while (std::getline(std::cin, tempStr)) {
+        std::size_t delimiter = tempStr.find('|');
+        std::stringstream uniqueSignalStream(tempStr.substr(0, delimiter));
+        std::stringstream outputValueStream(tempStr.substr(delimiter + 2));
+
+        
+
+        while (uniqueSignalStream >> tempStr) {
+            tempVec.emplace_back(tempStr);
+            auto it = &tempVec.back();
+            switch (it->setLength()) {
+            case ONE:
+                it->setNum(1);
+                break;
+            case FOUR:
+                it->setNum(4);
+                break;
+            case SEVEN:
+                it->setNum(7);
+                break;
+            case EIGHT:
+                it->setNum(8);
+                break;
+            }
+        }
+
+        while (outputValueStream >> tempStr)  { 
+            tempVec.emplace_back(tempStr);
+            auto it = &tempVec.back();
+            switch (it->setLength()) {
+            case ONE:
+                it->setNum(1);
+                break;
+            case FOUR:
+                it->setNum(4);
+                break;
+            case SEVEN:
+                it->setNum(7);
+                break;
+            case EIGHT:
+                it->setNum(8);
+                break;
+            }
+        }
+
+        digitalOutputs.push_back(tempVec);
+        tempVec.clear();
+    }
+
+    // Part One
+
+    uint32_t unique_numbers = 0, tempNum = 0;
+
+    for (const auto& line : digitalOutputs) {
+        for (uint32_t index = 10; index < 14; index++) {
+            switch(line[index].num()) {
+            case 1:
+                unique_numbers++;
+                break;
+            case 4:
+                unique_numbers++;
+                break;
+            case 7:
+                unique_numbers++;
+                break;
+            case 8:
+                unique_numbers++;
+                break;
+            }
+        }
+    }
+
+    // Part Two
+
+    uint64_t totalSum = 0;
+
+    for (auto& line : digitalOutputs) {
+        auto it_one = std::find(line.begin(), line.end(), 1);
+        auto it_four = std::find(line.begin(), line.end(), 4);
+
+        uint16_t index = 10;
+        uint16_t multiple = 1000;
+
+        for (; index < 14; index++) {
+            if (line[index].num() == 8) totalSum += 8 * multiple;
+            if (line[index].num() == 7) totalSum += 7 * multiple;
+            if (line[index].num() == 4) totalSum += 4 * multiple;
+            if (line[index].num() == 1) totalSum += 1 * multiple;
+
+            if (line[index].setLength() == 6) {
+                if (includes_set(line[index], *it_one)) {
+                    if (includes_count(line[index], *it_four) == 4) {
+                        totalSum += 9 * multiple;
+                    }
+                    else {
+                        totalSum += 0 * multiple;
+                    }
+                }
+                else {              
+                    totalSum += 6 * multiple;
+                }
+            }
+
+            if (line[index].setLength() == 5) { 
+                if (includes_set(line[index], *it_one)) {
+                    totalSum += 3 * multiple;
+                }
+                else {
+                    if (includes_count(line[index], *it_four) == 3) {
+                        totalSum += 5 * multiple;
+                    }
+                    else {
+                        totalSum += 2 * multiple;
+                    }
+                }
+            }
+
+            multiple /= 10;
+        }
+
+        std::cout << totalSum << std::endl;
+    }
+
+    std::cout << std::endl << "Part One:" << std::endl << unique_numbers << std::endl;
+    std::cout << "Part Two:" << std::endl << totalSum << std::endl;
+
+    return 0;
+}
---
+++ 2021/day08.hpp
@@ -0,0 +1,61 @@
+#ifndef DAY08_HPP
+#define DAY08_HPP
+
+#include <cstdint>
+#include <set>
+#include <string>
+#include <algorithm>
+
+class uniqueSignal {
+public:
+    uniqueSignal(const std::string& iniSignal);
+
+    inline uint16_t setLength() const { return signalConnections.size(); }
+    inline void setNum(uint16_t numIni) { number = numIni; }
+    inline uint16_t num() const { return number; }
+    inline std::set<char> returnSignal() const { return signalConnections; }
+
+    friend bool operator==(const uniqueSignal& lhs, const uniqueSignal& rhs);
+    friend bool operator==(const uniqueSignal& lhs, const uint64_t i);
+    friend bool includes_set(const uniqueSignal& lhs, const uniqueSignal& rhs);
+    friend uint16_t includes_count(const uniqueSignal& lhs, const uniqueSignal& rhs);
+
+private:
+    std::set<char> signalConnections;
+    uint16_t number = 10;
+};
+
+uniqueSignal::uniqueSignal(const std::string& iniSignal) {
+    for (const auto& connection : iniSignal) signalConnections.insert(connection);
+    return;
+}
+
+bool operator==(const uniqueSignal& lhs, const uniqueSignal& rhs) {
+    return (lhs.number == rhs.number) ? 1 : 0;
+}
+
+bool operator==(const uniqueSignal& lhs, const uint64_t i) {
+    return (lhs.number == i) ? 1 : 0;
+}
+
+bool includes_set(const uniqueSignal& lhs, const uniqueSignal& rhs) { // Ensure smaller set is on the rhs of the function call
+    if (std::includes(lhs.signalConnections.begin(), lhs.signalConnections.end(), rhs.signalConnections.begin(), rhs.signalConnections.end())) return true;
+    return false;
+}
+
+uint16_t includes_count(const uniqueSignal& lhs, const uniqueSignal& rhs) {
+    uint16_t count = 0;
+
+    auto min_it = (lhs.signalConnections.size() < rhs.signalConnections.size()) ? rhs.signalConnections.begin() : lhs.signalConnections.begin();
+    auto max_it = (lhs.signalConnections.size() < rhs.signalConnections.size()) ? rhs.signalConnections.end() : lhs.signalConnections.end();
+
+    auto fixedSet = (lhs.signalConnections.size() > rhs.signalConnections.size()) ? &rhs.signalConnections : &lhs.signalConnections;
+
+    for (; min_it != max_it; min_it++) {
+        count += fixedSet->count(*min_it);
+    }
+
+    return count;
+}
+
+#endif // DAY08_HPP
