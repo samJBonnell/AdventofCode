6d6d96b412134783064cf8ce27e426c4c567a163
samJBonnell <sbonnell147@gmail.com>
2021-12-12 15:42:27 -0800

    Finished day09 part 1

---
+++ 2021/day09.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <cstdint>
+
+bool check_adjacent(const std::multiset<uint16_t>& heights, const uint16_t& target);
+std::multiset<uint16_t> make_adjacent(const std::vector<std::vector<uint16_t>>& cave_floor, const uint16_t& row, const uint16_t& col);
+bool check_set(std::multiset<uint16_t>::iterator& it, std::multiset<uint16_t>::iterator& end, const uint16_t& target);
+
+int main(void) {
+    std::vector<std::vector<uint16_t>> cave_floor;
+    std::vector<uint16_t> tempVec;
+    std::string tempStr;
+
+    uint64_t total_risk = 0;
+
+    while (std::getline(std::cin, tempStr)) {
+        tempVec.push_back(9);
+        for (auto it = tempStr.begin(), eos = tempStr.end(); it != eos; it++) tempVec.push_back(*it - '0');
+        tempVec.push_back(9);
+        cave_floor.push_back(tempVec);
+        tempVec.clear();
+    }
+
+    size_t cave_width = cave_floor[0].size();
+    std::vector<uint16_t> paddingVec(cave_width, 9);
+    cave_floor.insert(cave_floor.begin(), paddingVec);
+    cave_floor.push_back(paddingVec);
+
+    for (uint16_t it_row = 1; it_row < cave_floor.size() - 1; it_row++) {
+        for (uint16_t it_col = 1; it_col < cave_floor[it_row].size() - 1; it_col++) {
+            if (check_adjacent(make_adjacent(cave_floor, it_row, it_col), cave_floor[it_row][it_col])) total_risk += cave_floor[it_row][it_col] + 1;
+        }
+    }
+
+    std::cout << "Part One:" << std::endl << total_risk << std::endl;
+
+    return 0;
+}
+
+bool check_adjacent(const std::multiset<uint16_t>& heights, const uint16_t& target) {
+    auto it = heights.begin(), end = heights.end();
+    if (*it++ == target && check_set(it, end, target)) return 1;
+    return 0;
+}
+
+bool check_set(std::multiset<uint16_t>::iterator& it, std::multiset<uint16_t>::iterator& end, const uint16_t& target) {
+    while (it != end) {
+        if (*it++ == target) return 0;
+    }
+
+    return 1;
+}
+
+std::multiset<uint16_t> make_adjacent(const std::vector<std::vector<uint16_t>>& cave_floor, const uint16_t& row, const uint16_t& col) {
+    std::multiset<uint16_t> tempSet;
+
+    for (int i = -1; i < 2; i++) tempSet.insert(cave_floor[row + i][col]); // Get the rows
+    for (int i = -1; i < 2; i+= 2) tempSet.insert(cave_floor[row][col + i]); // Get the cols
+
+    return tempSet;
+}
