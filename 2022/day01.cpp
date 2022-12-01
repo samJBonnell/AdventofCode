5ad4a0cbea365ed84926a36ac40baf40c7c264c3
samJBonnell <sbonnell147@gmail.com>
2022-11-30 22:30:07 -0800

    Advent Day 01

--- 2022/day01.cpp
+++ 2022/day01.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <iterator>
+#include <set>
+#include <string>
+#include <algorithm>
 
 int main(void) {
-    std::cout << "Hello Advent of Code 2022!" << std::endl;
-    std::cout << "This is from my second computer" << std::endl;
+    std::set<uint32_t> elfCalories;
+    std::string calorie;
+    uint32_t currentCalories = 0;
 
+    while (std::getline(std::cin, calorie)) {
+        if (calorie == "") {
+            elfCalories.insert(currentCalories);
+            currentCalories = 0;
+        }
+        else currentCalories += std::stoi(calorie);
+    }
+
+    elfCalories.insert(currentCalories);
+
+    // Part One
+    std::cout << "Part One:" << std::endl << *(elfCalories.rbegin()) << std::endl;
+
+    // Part Two
+    auto i = elfCalories.rbegin();
+    uint32_t tripleSum = *(i++) + *(i++) + *i;
+    std::cout << std::endl << "Part Two:" << std::endl << tripleSum << std::endl;
     return 0;
 }
