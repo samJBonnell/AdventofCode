ebb32f809a0c4e5d5a37b9b4b099a74580e4385b
unknown <sbonnell147@gmail.com>
2022-12-03 16:06:33 -0800

    First part of day 03

--- 2022/day03.cpp
+++ 2022/day03.cpp
@@ -1,44 +1,50 @@
 #include <iostream>
-#include <set>
 #include <vector>
 #include <string>
 #include <iterator>
 
-std::set<char> set_intersection(std::set<char>&, std::set<char>&);
+const uint32_t UPPERFACTOR = 38;
+const uint32_t LOWERFACTOR = 96;
+
+void uniqueChars(std::string& inputSet, const std::string& front);
 
 struct RuckSack {
-    std::set<char> front, back;
+    std::string front, back;
 };
 
 int main(void) {
     std::vector<RuckSack> RuckSacks;
-    std::string inputString, front, back;
     RuckSack tempSack;
+
+    std::string inputString, front, back, tempString;
+    uint32_t score = 0;
 
     while (std::getline(std::cin, inputString)) {
         front = inputString.substr(0, inputString.length()/2);
         back = inputString.substr(inputString.length()/2);
 
         for (uint16_t i = 0; i < front.length(); i++) {
-            tempSack.front.insert(front[i]);
-            tempSack.back.insert(front[i]);
+            tempSack.front.push_back(front[i]);
+            tempSack.back.push_back(back[i]);
         }
 
         RuckSacks.push_back(tempSack);
     }
 
     for (const auto& sack : RuckSacks) {
-        
+        uniqueChars(tempString, sack.front);
+        if (std::isupper(*tempString.begin())) score += *tempString.begin() - UPPERFACTOR;
+        else score += *tempString.begin() - LOWERFACTOR;
+        tempString.clear();
     }
+
+    std::cout << "Part One;" << std::endl << score << std::endl;
 
     return 0;
 }
 
-std::set<char> set_intersection(std::set<char>& front, std::set<char>& back) {
-    std::set<char> tempSet;
-    for (auto item = front.begin(); item != front.end(); item++) {
-        if (back.find(*item) != back.end()) tempSet.insert(*item);
+void uniqueChars(std::string& inputString, const std::string& front) {
+    for (const auto& character : front) {
+        if (front.find(character) == std::string::npos) inputString.push_back(character);
     }
-
-    return tempSet;
 }
