942ab8c560af8ce567a7ac1b028e750e4a5df543
unknown <sbonnell147@gmail.com>
2022-12-04 01:30:38 -0800

    day03 part 1 complete

--- 2022/day03.cpp
+++ 2022/day03.cpp
@@ -2,49 +2,81 @@
 #include <vector>
 #include <string>
 #include <iterator>
+#include <set>
 
 const uint32_t UPPERFACTOR = 38;
 const uint32_t LOWERFACTOR = 96;
 
-void uniqueChars(std::string& inputSet, const std::string& front);
+void uniqueChars(char& returnChar, const std::set<char> front, const std::set<char> back);
+void commonSet(std::set<char>& returnSet, const std::set<char>& front, const std::set<char>& back);
 
 struct RuckSack {
-    std::string front, back;
+    std::set<char> front, back;
+    std::set<char> total;
 };
 
 int main(void) {
     std::vector<RuckSack> RuckSacks;
     RuckSack tempSack;
 
-    std::string inputString, front, back, tempString;
+    std::string inputString, front, back;
     uint32_t score = 0;
+    char tempChar;
 
     while (std::getline(std::cin, inputString)) {
         front = inputString.substr(0, inputString.length()/2);
         back = inputString.substr(inputString.length()/2);
 
+        for (const auto& character : inputString) {
+            tempSack.total.insert(character);
+        }
+
         for (uint16_t i = 0; i < front.length(); i++) {
-            tempSack.front.push_back(front[i]);
-            tempSack.back.push_back(back[i]);
+            tempSack.front.insert(front[i]);
+            tempSack.back.insert(back[i]);
         }
 
         RuckSacks.push_back(tempSack);
+        tempSack.front.clear();
+        tempSack.back.clear();
+        tempSack.total.clear();
     }
 
+    // Part One
     for (const auto& sack : RuckSacks) {
-        uniqueChars(tempString, sack.front);
-        if (std::isupper(*tempString.begin())) score += *tempString.begin() - UPPERFACTOR;
-        else score += *tempString.begin() - LOWERFACTOR;
-        tempString.clear();
+        uniqueChars(tempChar, sack.front, sack.back);
+        if (std::isupper(tempChar)) score += tempChar - UPPERFACTOR;
+        else score += tempChar - LOWERFACTOR;
+    }
+
+    // Part Two
+    std::set<char> tempSet;
+    uint32_t score2 = 0;
+
+    for (auto sack = RuckSacks.begin(); sack != RuckSacks.end(); sack + 3) {
+        commonSet(tempSet, (*(sack++)).total, (*(sack++)).total);
+        commonSet(tempSet, (*(sack++)).total, tempSet);
+
+        if (std::isupper(*tempSet.begin())) score2 += *tempSet.begin() - UPPERFACTOR;
+        else score2 += *tempSet.begin() - LOWERFACTOR;
+
+        tempSet.clear();
     }
 
     std::cout << "Part One;" << std::endl << score << std::endl;
+    std::cout << "Part Two;" << std::endl << score2 << std::endl;
 
     return 0;
 }
 
-void uniqueChars(std::string& inputString, const std::string& front) {
-    for (const auto& character : front) {
-        if (front.find(character) == std::string::npos) inputString.push_back(character);
+void uniqueChars(char& returnChar, const std::set<char> front, const std::set<char> back) {
+    for (const auto& item : front) {
+        if (back.find(item) != back.end()) returnChar = item;
+    }
+}
+
+void commonSet(std::set<char>& returnSet, const std::set<char>& front, const std::set<char>& back) {
+    for (const auto& item : front) {
+        if (back.find(item) != back.end()) returnSet.insert(item);
     }
 }
