f2c018a55e6e558c8caeeef7b844f47cf97d5c84
unknown <sbonnell147@gmail.com>
2022-12-28 00:27:25 -0800

    general tree configuration

--- 2022/day07.cpp
+++ 2022/day07.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
 
-#include "2022/day07.hpp"
+#include "day07.hpp"
 
 int main(void) {
+    std::istream_iterator<std::string> in(std::cin), end;
+    std::vector<std::string> terminalOutput(in, end);
 
+    for (const auto& line : terminalOutput) {
+        
+    }
 
     return 0;
 }
--- 2022/day07.hpp
+++ 2022/day07.hpp
@@ -18,12 +18,67 @@ public:
     inline void addSubDirectory(Directory* directory) {subDirectories.push_back(directory); };
     inline void addFiles(File file) {files.push_back(file); };
 
+    inline std::ostream& outputName(std::ostream& stream) { 
+        stream << this->name;
+        return stream;
+    };
 
+    friend void searchDirectories(Directory* root, const std::string target, bool& found);
+    friend uint64_t sumDirectories(Directory* root);
+    friend bool operator==(const Directory* lhs, const std::string& rhs);
+    friend bool operator==(const Directory lhs, const std::string& rhs);
+    friend bool operator!=(const Directory* lhs, const std::string& rhs);
 
 private:
     std::string name;
     std::vector<Directory *> subDirectories;
     std::vector<File> files;
 };
+
+bool operator==(const Directory* lhs, const std::string& rhs) {
+    return (lhs->name == rhs) ? 1 : 0;
+}
+
+bool operator==(const Directory lhs, const std::string& rhs) {
+    return (lhs.name == rhs) ? 1 : 0;
+}
+
+bool operator!=(const Directory* lhs, const std::string& rhs) {
+    return (lhs->name != rhs) ? 1 : 0;
+}
+
+void searchDirectories(Directory* root, const std::string target, bool& found) {
+    if (root == target) found = true;
+    for (const auto dir : root->subDirectories) {
+        if (dir == target) { // If the directory is the correct one
+            root = dir;
+            found = true;
+            return;
+        }
+        searchDirectories(dir, target, found);
+        if (found) return;
+    }
+}
+
+uint64_t sumDirectories(Directory* root) {
+    uint64_t tempSum = 0;
+    for (const auto dir : root->subDirectories) {
+        tempSum += sumDirectories(dir);
+    }
+
+    for (const auto& file : root->files) {
+        tempSum += file.size;
+    }
+
+    return tempSum;
+}
+
+File createFile(std::string inName, uint64_t inSize) {
+    File tempFile;
+    tempFile.name = inName;
+    tempFile.size = inSize;
+
+    return tempFile;
+}
 
 #endif
