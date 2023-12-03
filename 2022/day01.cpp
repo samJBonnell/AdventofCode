e3446c20fbdac9b46e6ea169e91c414fb7de1800
unknown <sbonnell147@gmail.com>
2023-12-02 21:09:34 -0800

    dfalkj

--- 2022/day01.cpp
+++ 2022/day01.cpp
@@ -3,12 +3,14 @@
 #include <set>
 #include <string>
 #include <algorithm>
+#include <chrono>
 
 int main(void) {
     std::set<uint32_t> elfCalories;
     std::string calorie;
     uint32_t currentCalories = 0;
 
+    auto start = std::chrono::high_resolution_clock::now();
     while (std::getline(std::cin, calorie)) {
         if (calorie == "") {
             elfCalories.insert(currentCalories);
@@ -26,5 +28,9 @@ int main(void) {
     auto i = elfCalories.rbegin();
     uint32_t tripleSum = *(i++) + *(i++) + *i;
     std::cout << std::endl << "Part Two:" << std::endl << tripleSum << std::endl;
+    
+    auto stop = std::chrono::high_resolution_clock::now();   
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << duration.count() << std::endl;
     return 0;
 }
